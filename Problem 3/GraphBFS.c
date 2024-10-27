341798bb70b68a9ff110108536d3ba12667a18c3
CX330Blake <timchen975@gmail.com>
2024-10-27 09:49:35 +0800

    .

--- Problem 3/GraphBFS.c
+++ Problem 3/GraphBFS.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct Queue {
+    int front, rear, size;
+    unsigned capacity;
+    int* data;
+} Queue;
+
+Queue* createQueue(unsigned capacity) {
+    Queue* queue = (Queue*)malloc(sizeof(Queue));
+    queue->capacity = capacity;
+    queue->front = 0;
+    queue->size = 0;
+    queue->rear = capacity - 1;
+    queue->data = (int*)malloc(queue->capacity * sizeof(int));
+    return queue;
+}
+
+int isFull(Queue* queue) { return queue->size == queue->capacity; }
+
+int isEmpty(Queue* queue) { return queue->size == 0; }
+
+void enqueue(Queue* queue, int item) {
+    if (isFull(queue)) return;
+    queue->rear = (queue->rear + 1) % queue->capacity;
+    queue->data[queue->rear] = item;
+    queue->size++;
+}
+
+int dequeue(Queue* queue) {
+    if (isEmpty(queue)) {
+        return -1;
+    }
+    int item = queue->data[queue->front];
+    // Make it a circular queue
+    queue->front = (queue->front + 1) % queue->capacity;
+    queue->size--;
+    return item;
+}
+
 int getNum() {
     int num;
     scanf("%d", &num);
@@ -20,7 +58,10 @@ int** createAdjMatrix(int num) {
     return adjMatrix;
 }
 
+int* BFS(int** adjMatrix) {}
+
 int main() {
     int num = getNum();
     int** adjMatrix = createAdjMatrix(num);
+    Queue* queue = createQueue(num);
 }
